9ab42a4935975cdcb1ae76d6c40aa3eedf66d4ca
webdeveloper0429 <webdeveloper0429@yahoo.com>
2022-04-22 00:46:20 -0400

    add controller

---
+++ src/Controller/VehicleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VehicleController extends AbstractController
+{
+    #[Route('/vehicle', name: 'app_vehicle')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/VehicleController.php',
+        ]);
+    }
+
+}
